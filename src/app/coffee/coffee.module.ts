cafdf06c6554edb08a3d275b28c537cc3774b434
Cristian Camilo Rojas <cristiancamilo.rojas@endava.com>
2022-05-01 15:47:30 -0500

    Add coffee module

---
+++ src/app/coffee/coffee.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CoffeeListComponent } from './coffee-list/coffee-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    CoffeeListComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports:[
+    CoffeeListComponent
+  ]
+})
+export class CoffeeModule { }
